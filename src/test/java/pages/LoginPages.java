e666a953f93e6f27a9919f7f95ba3147a8b6e699
mehediriseuplabs <hasan.mehedi.riseuplabs@gmail.com>
2022-09-25 13:10:56 +0600

    modified

--- src/test/java/pages/LoginPages.java
+++ src/test/java/pages/LoginPages.java
@@ -1,45 +1,56 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPages {
     protected WebDriver driver;
 
     public LoginPages(WebDriver driver) {
         this.driver = driver;
+
+        //init page factory
+        PageFactory.initElements(driver, this);
+
         if (!driver.getTitle().equals("TestProject Demo")) {
-            throw new IllegalStateException("This is not Login Page." +
-                    "The current page is" + driver.getCurrentUrl());
+            throw new IllegalStateException("This is not Login Page."
+                    + "The current page is" + driver.getCurrentUrl());
         }
     }
 
-    private By username = By.id("name");
-    private By password = By.id("password");
-    private By loginBtn = By.id("login");
-    private By validateBtn = By.id("logout");
+    @FindBy(id = "name")
+    WebElement username;
+
+    @FindBy(id = "password")
+    WebElement password;
+
+    @FindBy(id = "login")
+    WebElement loginBtn;
+    @FindBy(id = "logout")
+    WebElement validateBtn;
+
 
     public void enterUser(String name) {
-        driver.findElement(username).sendKeys(name);
+        username.sendKeys(name);
     }
 
     public void enterPassword(String passcode) {
-        driver.findElement(password).sendKeys(passcode);
+        password.sendKeys(passcode);
     }
 
     public void loginBtn() {
-        driver.findElement(loginBtn).click();
+        loginBtn.click();
     }
 
     public void validate() {
-        if (driver.findElement(validateBtn).isDisplayed()) {
-            driver.findElement(validateBtn).click();
+        if (validateBtn.isDisplayed()) {
+            validateBtn.click();
         } else {
             System.out.println("Something went wrong!");
             driver.quit();
         }
-
-
     }
 
 }
